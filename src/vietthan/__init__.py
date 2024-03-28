3aed9d5e55328dae9d5f6d8f4f6a4dc8fb3fe107
Viet Than <thanhoangviet@gmail.com>
2024-03-27 22:11:48 -0400

    got tox to pass

--- src/vietthan/__init__.py
+++ src/vietthan/__init__.py
@@ -1,2 +1,13 @@
+"""a test pypi module by VietThan"""
+
+
 def name() -> str:
+    """
+    Returns "vietthan"
+
+    Returns
+    -------
+    str
+        "vietthan"
+    """
     return "vietthan"
